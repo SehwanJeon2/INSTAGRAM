5f10cab215ef1eed3233abc4ed6733e53b6ced3a
Jeon Sehwan <jeonsewall@hanyang.ac.kr>
2019-04-10 04:48:27 +0000

    Post 삭제 기능 추가

--- posts/views.py
+++ posts/views.py
@@ -28,5 +28,7 @@ def list(request):
     }
     return render(request, 'posts/list.html', context)
 
-def delete(request):
-    pass
+def delete(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    post.delete()
+    return redirect('posts:list')
