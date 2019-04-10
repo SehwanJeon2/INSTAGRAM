4b6d8adc0f70e6378c0c41e1c84823b415cb7de3
Jeon Sehwan <jeonsewall@hanyang.ac.kr>
2019-04-10 02:46:01 +0000

    bootstrap4 설치 및 form에 적용 & post 작성 view 추가

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import PostModelForm
 
 # Create your views here.
@@ -6,7 +6,10 @@ def create(request):
     # 만약 POST 요청이 오면
     if request.method == 'POST':
         # 글을 작성하기
-        pass
+        form = PostModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:create')
     # 아니면 GET 요청이 오면
     else:
         # post를 작성하는 폼을 가져와 template에서 보여줌.
